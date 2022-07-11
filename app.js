74fa97debfaef145287e215b8fe1447d1c059b4c
Patrycja-dz <dziemianko.pat@gmail.com>
2022-07-11 22:44:46 +0200

    wrote summary function

--- app.js
+++ app.js
@@ -1,46 +1,44 @@
-{
+const qs = (selector) => document.querySelector(selector)
+const qsAll = (selectors) => document.querySelectorAll(selectors)
+const ce = (tag) => document.createElement(tag)
 
-    const qId = (id) => document.getElementById(id)
-    const qsAll = (selectors) => document.querySelectorAll(selectors)
-    const ce = (tag) => document.createElement(tag)
-
-    const getIndexOfChosenRating = () => {
-        const allRatingsStars = qsAll('.rating-item');
-        allRatingsStars.forEach((star, index) => chosenRating(star, index))
-
-    }
-
-    const chosenRating = (element, index) => {
-        let result = index + 1;
-        element.addEventListener('click', (e) => {
-            e.stopPropagation();
-            createThanksForRatingModal(result)
-        })
-        
-    }
-
-    const submitRating = () => {
-        const submitBtn = qId('js-submit');
-   
-    }
-
-    const createThanksForRatingModal = (result) =>{
-        const section = ce('section');
-        section.classList.add('thanks-state');
-        section.style.height='230px'
-
-        const paragraph = ce('p')
-        paragraph.textContent = `you selected ${result} from 5`
-        section.appendChild(paragraph)
-        console.log(paragraph)
-    }
-    createThanksForRatingModal()
-
-    submitRating()
-    getIndexOfChosenRating()
-
-
-
-
-
+const getIndexOfChosenRating = () => {
+    const allRatingsStars = qsAll('.rating-item');
+    allRatingsStars.forEach((star, index) => chosenRating(star, index))
 }
+
+const chosenRating = (element, index) => {
+    let result = index + 1;
+    element.addEventListener('click', () => {
+        element.style.backgroundColor = '#fb7413';
+        console.log(result)
+        feedback(result)
+    })
+}
+const feedback = (rate) => {
+    let feedbackText = document.getElementById('selected-rating');
+    feedbackText.textContent = `You selected ${rate} out of 5`
+}
+
+const submitRating = (e) => {
+    changeView()
+    
+}
+
+const changeView = () => {
+    const ratingSection = qs('.rating');
+    ratingSection.classList.add('hidden');
+    const summarySection = qs('.feedback');
+    summarySection.classList.add('visible');
+}
+const submitBtn = document.getElementById("submitBtn")
+submitBtn.addEventListener('click', (submitRating))
+
+
+
+
+
+
+
+
+getIndexOfChosenRating()
